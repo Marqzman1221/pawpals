9ef9beae96ec0c95ac21e323d4453ddb6a304a2d
tgmarqui <tgmarqui@ncsu.edu>
2021-05-11 20:35:38 -0400

    Button, Card, Toolbar, Autocomplete components

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="bg-white p-6 flex flex-row justify-between items-center w-full h-16"
+  >
+    <div>
+      <slot name="toolbar-icon"> </slot>
+
+      <slot name="toolbar-title"> </slot>
+    </div>
+
+    <div>
+      <slot name="toolbar-actions"> </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  color: {
+    type: String,
+    default: '#ffffff',
+  },
+})
+</script>
+
+<style></style>
