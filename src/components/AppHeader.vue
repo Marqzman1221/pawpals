dfd5af5c4f9b0f8651af2a7b74e641acd6fad767
tgmarqui <tgmarqui@ncsu.edu>
2021-05-11 20:47:45 -0400

    New AppHeader component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="fixed top-0 left-0 w-screen">
+    <toolbar>
+      <template #toolbar-title>
+        <div class="text-pink-600 text-2xl text-bold">PAWPALS</div>
+      </template>
+
+      <template #toolbar-actions>
+        <Button>GitHub</Button>
+      </template>
+    </toolbar>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import Button from './Button.vue'
+import Toolbar from './Toolbar.vue'
+
+export default defineComponent({
+  components: { Toolbar, Button },
+})
+</script>
