96a0af84c9551c2911c93d9f92523a5e645283a5
tgmarqui <tgmarqui@ncsu.edu>
2021-05-13 11:47:18 -0400

    Added AppHeader, fixing router-view positioning

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="flex flex-col">
-    <!-- <app-header /> -->
+  <app-header />
+  <div class="absolute top-16 w-full overflow-y-auto">
     <router-view />
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-// import AppHeader from './components/AppHeader'
+import AppHeader from './components/AppHeader'
 
 export default defineComponent({
   components: {
-    // AppHeader,
+    AppHeader,
   },
 })
 </script>
