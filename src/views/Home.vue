b0ca0d2625dc35c70ed349a22505b01005c7bb98
tgmarqui <tgmarqui@ncsu.edu>
2021-05-13 11:48:05 -0400

    Working on column structure of Home screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,84 @@
 <template>
-  <filter-card />
+  <div class="flex flex-row justify-center">
+    <div class="relative flex-row-reverse max-w-7xl justify-end">
+      <div class="flex relative">
+        <filter-card class="flex fixed max-w-md" />
+      </div>
+    </div>
+    <div class="flex-grow max-w-screen-md justify-self-center">
+      <card>
+        <template #card-title>Hello</template>
+
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
+        </div>
+      </card>
+      <card>
+        <template #card-title>Hello</template>
+
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
+        </div>
+      </card>
+      <card>
+        <template #card-title>Hello</template>
+
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
+        </div>
+      </card>
+      <card>
+        <template #card-title>Hello</template>
+
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
+        </div>
+      </card>
+      <card>
+        <template #card-title>Hello</template>
+
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
+        </div>
+      </card>
+      <card>
+        <template #card-title>Hello</template>
+
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
+        </div>
+      </card>
+      <card>
+        <template #card-title>Hello</template>
+
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
+        </div>
+      </card>
+      <card>
+        <template #card-title>Hello</template>
+
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
+        </div>
+      </card>
+    </div>
+    <div class="flex flex-row max-w-7xl justify-start">
+      <div class="relative">
+        <filter-card class="fixed max-w-md" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from '@vue/runtime-core'
+import Card from '../components/Card.vue'
 // import Card from '../components/Card.vue'
 import FilterCard from '../components/FilterCard.vue'
 // import Toolbar from '../components/Toolbar.vue'
 
 export default defineComponent({
-  components: { FilterCard },
-  // TODO
+  components: { FilterCard, Card },
 })
 </script>
