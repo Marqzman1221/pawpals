7576ea8f29f9dc69be4b7f3bacaa3eb9c707d5fc
tgmarqui <tgmarqui@ncsu.edu>
2021-05-13 11:36:57 -0400

    Implemented AppHeader's content

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,23 +1,35 @@
 <template>
-  <div class="fixed top-0 left-0 w-screen">
+  <header class="fixed top-0 left-0 w-screen shadow z-1000">
     <toolbar>
-      <template #toolbar-title>
-        <div class="text-pink-600 text-2xl text-bold">PAWPALS</div>
+      <template #toolbar-icon>
+        <Icon size="lg" :path="mdiPaw" />
       </template>
 
+      <template #toolbar-title>PAWPALS</template>
+
       <template #toolbar-actions>
-        <Button>GitHub</Button>
+        <Button icon>
+          <icon size="lg" :path="mdiGithub" />
+        </Button>
       </template>
     </toolbar>
-  </div>
+  </header>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import Button from './Button.vue'
+import Icon from './Icon.vue'
 import Toolbar from './Toolbar.vue'
+import { mdiPaw, mdiGithub } from '@mdi/js'
 
 export default defineComponent({
-  components: { Toolbar, Button },
+  components: { Toolbar, Button, Icon },
+  setup() {
+    return {
+      mdiPaw,
+      mdiGithub,
+    }
+  },
 })
 </script>
