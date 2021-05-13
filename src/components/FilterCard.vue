2990f44b2ae3341ea93e6afa97e8ee6880ad4544
tgmarqui <tgmarqui@ncsu.edu>
2021-05-13 11:46:37 -0400

    Refined content of FilterCard header

--- src/components/FilterCard.vue
+++ src/components/FilterCard.vue
@@ -1,6 +1,13 @@
 <template>
   <card>
-    <template #card-title>Filter</template>
+    <template #card-title>
+      <span class="inline-flex items-center w-full">
+        <span class="flex-grow">Filter</span>
+        <Button class="justify-self-end" icon>
+          <icon :path="mdiReload" />
+        </Button>
+      </span>
+    </template>
 
     <pet-filter-form />
 
@@ -15,15 +22,18 @@ import { defineComponent } from 'vue'
 import Card from './Card.vue'
 import PetFilterForm from './PetFilterForm.vue'
 import Button from './Button.vue'
+import Icon from './Icon.vue'
+import { mdiReload } from '@mdi/js'
 
 export default defineComponent({
-  components: { Card, PetFilterForm, Button },
+  components: { Card, PetFilterForm, Button, Icon },
   setup() {
     function submitFilter() {
       console.log('Filter')
     }
 
     return {
+      mdiReload,
       submitFilter,
     }
   }
