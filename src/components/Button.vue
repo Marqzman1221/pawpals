4635bfdeca13402ee78c25a10e8e7990acdb05c8
tgmarqui <tgmarqui@ncsu.edu>
2021-05-13 11:44:27 -0400

    Added flat, disabled props; refined styling

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,7 +1,15 @@
 <template>
   <button
-    class="rounded-full font-bold transition duration-200 ease-in-out"
+    class="
+      outline-none
+      rounded-full
+      font-bold
+      transition
+      duration-200
+      ease-in-out
+    "
     :class="classes"
+    :disabled="disabled"
   >
     <slot></slot>
   </button>
@@ -28,20 +36,54 @@ export default defineComponent({
       type: Boolean,
       default: false,
     },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    flat: {
+      type: Boolean,
+      default: false,
+    }
   },
   setup(props) {
+    const backgroundColor = computed(() => {
+      if (props.icon) {
+        return {
+          hover: `gray-200`,
+          base: 'transparent',
+          active: 'gray-400',
+        }
+      }
+
+      if (props.flat || props.outlined) {
+        return {
+          hover: 'transparent',
+          base: 'transparent',
+          active: 'transparent',
+        }
+      }
+
+      return {
+        hover: `${props.color}-400`,
+        base: `${props.color}-600`,
+        active: `${props.color}-800`,
+      }
+    })
+
     const classes = computed(() => {
-      let classString = ''
+      let classString = `bg-${backgroundColor.value['base']} hover:bg-${backgroundColor.value['hover']} active:bg-${backgroundColor.value['active']} focus:outline-none active:outline-none`
 
       if (props.outlined)
         classString += ` text-${props.color}-600 hover:text-${props.color}-800 active:text-${props.color}-400 border-2 border-${props.color}-600 hover:border-${props.color}-800 active:border-${props.color}-400`
-      else
-        classString += ` text-white bg-${props.color}-600 hover:bg-${props.color}-800 active:bg-${props.color}-400`
+      else if (!props.icon) classString += ` text-white`
 
-      if (props.icon) classString += ` p-2`
+      if (props.icon) classString += ` text-${props.color}-600 p-2`
       else classString += ` px-6 py-2`
 
       if (props.block) classString += ` w-full`
+
+      if (props.disabled)
+        classString += ' disabled:opacity-50 cursor-not-allowed'
 
       return classString
     })
