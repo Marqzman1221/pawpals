7ead8dadb5c236c97d5afbf4753ae93f8c45b3e1
tgmarqui <tgmarqui@ncsu.edu>
2021-05-17 10:38:41 -0400

    Added responsiveness and RecentlyViewedCard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,84 +1,52 @@
 <template>
-  <div class="flex flex-row justify-center">
-    <div class="relative flex-row-reverse max-w-7xl justify-end">
-      <div class="flex relative">
-        <filter-card class="flex fixed max-w-md" />
+  <div
+    class="
+      flex
+      xl:max-w-screen-xl
+      lg:max-w-screen-lg
+      sm:max-w-screen-sm
+      flex-grow
+      lg:my-2
+    "
+  >
+    <div v-show="lgAndGreater" class="justify-end">
+      <pet-search-card class="flex w-72" />
+    </div>
+    <div class="flex-grow justify-center">
+      <div class="overflow-y-auto">
+        <pets-list />
       </div>
     </div>
-    <div class="flex-grow max-w-screen-md justify-self-center">
-      <card>
-        <template #card-title>Hello</template>
-
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
-        </div>
-      </card>
-      <card>
-        <template #card-title>Hello</template>
-
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
-        </div>
-      </card>
-      <card>
-        <template #card-title>Hello</template>
-
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
-        </div>
-      </card>
-      <card>
-        <template #card-title>Hello</template>
-
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
-        </div>
-      </card>
-      <card>
-        <template #card-title>Hello</template>
-
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
-        </div>
-      </card>
-      <card>
-        <template #card-title>Hello</template>
-
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
-        </div>
-      </card>
-      <card>
-        <template #card-title>Hello</template>
-
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
-        </div>
-      </card>
-      <card>
-        <template #card-title>Hello</template>
-
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida. Gravida quis blandit turpis cursus in hac habitasse platea dictumst. Quisque non tellus orci ac auctor augue. Nibh ipsum consequat nisl vel pretium lectus quam. Phasellus faucibus scelerisque eleifend donec. Tellus mauris a diam maecenas sed. Ut venenatis tellus in metus vulputate eu scelerisque felis. Vitae auctor eu augue ut lectus arcu bibendum. Consectetur a erat nam at. Vestibulum sed arcu non odio euismod lacinia at. Semper quis lectus nulla at volutpat diam ut.
-        </div>
-      </card>
-    </div>
-    <div class="flex flex-row max-w-7xl justify-start">
-      <div class="relative">
-        <filter-card class="fixed max-w-md" />
-      </div>
+    <div v-show="xlAndGreater" class="justify-start">
+      <recently-viewed-card v-if="recentlyViewed" class="flex w-72" />
+      <div v-else class="flex m-2 w-72" />
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from '@vue/runtime-core'
-import Card from '../components/Card.vue'
-// import Card from '../components/Card.vue'
-import FilterCard from '../components/FilterCard.vue'
-// import Toolbar from '../components/Toolbar.vue'
+import { defineComponent } from 'vue'
+import { useBreakpoints, breakpointsTailwind } from '@vueuse/core'
+import PetSearchCard from '../components/PetSearchCard.vue'
+import PetsList from '@/components/PetsList'
+import RecentlyViewedCard from '@/components/RecentlyViewedCard'
+import { usePets } from '@/compositions/pets'
 
 export default defineComponent({
-  components: { FilterCard, Card },
+  components: { PetSearchCard, PetsList, RecentlyViewedCard },
+  setup() {
+    const { recentlyViewed } = usePets()
+    const breakpoints = useBreakpoints(breakpointsTailwind)
+
+    const lgAndGreater = breakpoints.greater('lg')
+    const xlAndGreater = breakpoints.greater('xl')
+
+    return {
+      breakpoints,
+      recentlyViewed,
+      lgAndGreater,
+      xlAndGreater,
+    }
+  },
 })
 </script>
