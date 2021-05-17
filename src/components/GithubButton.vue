ce4d6c9dcff9a26be0cae2104e6fcc09b3513221
tgmarqui <tgmarqui@ncsu.edu>
2021-05-17 09:51:59 -0400

    New GithubButton component

---
+++ src/components/GithubButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <Button icon>
+    <icon size="lg" :path="mdiGithub" @click="openRepoPage()" />
+  </Button>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import Button from './Button'
+import Icon from './Icon'
+import { mdiGithub } from '@mdi/js'
+
+export default defineComponent({
+  components: { Button, Icon },
+  setup() {
+    function openRepoPage() {
+      console.log('Open Repo Page')
+      window.open('https://github.com/Marqzman1221/pawpals', '_blank')
+    }
+
+    return {
+      openRepoPage,
+      mdiGithub,
+    }
+  },
+})
+</script>
