a042033a28337599bc23ef6b77190f8cd60895b2
tgmarqui <tgmarqui@ncsu.edu>
2021-05-17 10:20:26 -0400

    Modified responsive margins

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="bg-white m-2 rounded border-2 border-gray-200">
-    <div class="m-6">
+  <div class="flex bg-white m-2 rounded border-2 border-gray-200">
+    <div class="sm:m-6 m-4 flex-grow">
       <div class="flex text-2xl">
         <slot name="card-title"></slot>
       </div>
 
       <slot></slot>
 
-      <div class="mt-6">
-        <slot name="card-actions"></slot>
-      </div>
+      <slot class="mt-6" name="card-actions"></slot>
     </div>
   </div>
 </template>
