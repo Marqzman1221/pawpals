8aff3ec1c08797b1d662360ebdf6299cc9036c55
tgmarqui <tgmarqui@ncsu.edu>
2021-05-17 10:36:18 -0400

    Added pagination support and recentlyViewed

--- src/compositions/pets.js
+++ src/compositions/pets.js
@@ -1,27 +1,28 @@
-import { reactive, readonly } from 'vue'
+import { reactive, ref, computed, toRaw, readonly } from 'vue'
 import api from '../api'
 
+const loadingList = ref(true)
 const petsList = reactive([])
 const focusedPet = reactive({})
 const recentlyViewed = reactive([])
 
-const typesList = reactive(['Rabbit', 'Dog', 'Cat', 'Bird', 'Lizard', 'Fish'])
-const locationsList = reactive([
-  'Raleigh',
-  'New York City',
-  'Atlanta',
-  'Los Angeles',
-  'Denver',
-  'Seattle',
-])
+const typesList = reactive([])
+const locationsList = reactive([])
 
-const filters = reactive({
-  type: '',
-  location: '',
+const params = reactive({
+  type: 'Rabbit',
+  location: 'Raleigh, NC',
+  page: 1,
 })
+
+let queriedParams = { ...toRaw(params) }
 
 function SET_PETS_LIST(list) {
   petsList.value = list
+}
+
+function APPEND_PETS_LIST(list) {
+  petsList.value = [...petsList.value, ...list]
 }
 
 function SET_FOCUSED_PET(pet) {
@@ -37,25 +38,56 @@ function SET_LOCATIONS_LIST(list) {
 }
 
 function APPEND_RECENTLY_VIEWED(pet) {
-  recentlyViewed.push(pet)
+  let recents = toRaw(recentlyViewed)
+
+  const foundIndex = recents.findIndex((item) => item.id === pet.id)
+
+  if (foundIndex > -1) recents.splice(foundIndex, 1)
+
+  recents.unshift(pet)
+
+  if (recents.length > 5) recents.splice(5)
+
+  recentlyViewed.value = recents
+}
+
+function SAVE_QUERIED_FILTERS(params) {
+  queriedParams = params
 }
 
 async function fetchPets() {
   try {
-    const data = getPetFilters()
+    loadingList.value = true
+    const data = getPetParams()
     const response = await api.getAnimals(data)
+
     SET_PETS_LIST(response.animals)
+    SAVE_QUERIED_FILTERS(data)
+    loadingList.value = false
   } catch (error) {
-    console.log(error)
+    throw new Error(error)
+  }
+}
+
+async function fetchPetsAppend() {
+  try {
+    params.page += 1
+    const data = getPetParams()
+    const response = await api.getAnimals(data)
+
+    APPEND_PETS_LIST(response.animals)
+  } catch (error) {
+    throw new Error(error)
   }
 }
 
 async function fetchPetByID(id) {
   try {
     const response = await api.getAnimalByID(id)
+
     SET_FOCUSED_PET(response.animal)
   } catch (error) {
-    console.log(error)
+    throw new Error(error)
   }
 }
 
@@ -66,20 +98,20 @@ async function fetchTypes() {
 
     SET_TYPES_LIST(types)
   } catch (error) {
-    console.log
+    console.error(error)
   }
 }
 
-async function fetchLocations() {
+async function fetchLocations(prefix) {
   try {
-    const response = await api.getOrganizations()
-    const locations = response.organizations.map((item) => {
-      return `${item.address.city}, ${item.address.state}`
+    const response = await api.getLocations(prefix)
+    const locations = response.map((item) => {
+      return `${item.value}, ${item.state_code.substr(3, 2)}`
     })
 
     SET_LOCATIONS_LIST(locations)
   } catch (error) {
-    console.log(error)
+    console.error(error)
   }
 }
 
@@ -87,24 +119,28 @@ function appendRecentlyViewed(pet) {
   APPEND_RECENTLY_VIEWED(pet)
 }
 
-const getPetFilters = () => ({
-  type: filters.type,
-  location: filters.location,
+const getPetParams = () => ({
+  type: params.type,
+  location: params.location,
+  page: params.page,
 })
 
 export const usePets = () => ({
   // State
-  filters,
+  params,
   // Actions
   fetchPets,
+  fetchPetsAppend,
   fetchPetByID,
   fetchTypes,
   fetchLocations,
   appendRecentlyViewed,
   // Getters
-  petsList: readonly(petsList),
-  focusedPet: readonly(focusedPet),
-  recentlyViewed: readonly(recentlyViewed),
-  typesList: readonly(typesList),
-  locationsList: readonly(locationsList),
+  queriedParams: readonly(queriedParams),
+  loadingList: computed(() => loadingList.value),
+  petsList: computed(() => petsList.value),
+  focusedPet: computed(() => focusedPet.value),
+  recentlyViewed: computed(() => recentlyViewed.value),
+  typesList: computed(() => typesList.value),
+  locationsList: computed(() => locationsList.value),
 })
