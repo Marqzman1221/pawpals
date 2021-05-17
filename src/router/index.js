334af32963e229a8cfc06f29d8f6deec2e52b6fa
tgmarqui <tgmarqui@ncsu.edu>
2021-05-17 10:36:36 -0400

    Added route for PetDetail view

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home'
+import PetDetail from '@/views/PetDetail'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/pet/:id',
+    name: 'Pet',
+    component: PetDetail,
   },
 ]
 
